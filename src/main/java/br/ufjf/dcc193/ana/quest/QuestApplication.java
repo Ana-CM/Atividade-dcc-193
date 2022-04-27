baeccbe0b8df7c8beaf7262073bfc08dc8eb7856
ana <ana.mendes@ice.ufjf.br>
2022-04-26 22:48:00 -0300

    Criando a segunda missao

--- src/main/java/br/ufjf/dcc193/ana/quest/QuestApplication.java
+++ src/main/java/br/ufjf/dcc193/ana/quest/QuestApplication.java
@@ -14,8 +14,13 @@ public class QuestApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(QuestApplication.class, args);
 		System.out.println("Hora da Aventura");
+		
 		Missao m1 = ctx.getBean(Missao.class);
 		m1.iniciar();
 		m1.concluir(); 
+
+		Missao m2 = ctx.getBean(Missao.class);
+		m2.iniciar();
+		m2.concluir(); 
 	}
 }
